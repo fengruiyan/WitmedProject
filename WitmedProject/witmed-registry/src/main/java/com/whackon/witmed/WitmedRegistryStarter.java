caee04ddc4b32316ee33f259b9045b2fe87b9dfa
闫瑞锋 <2877664730@qq.com>
2021-12-29 23:21:00 +0800

    创建项目

---
+++ WitmedProject/witmed-registry/src/main/java/com/whackon/witmed/WitmedRegistryStarter.java
@@ -0,0 +1,20 @@
+package com.whackon.witmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <b>智慧医疗信息平台-Eureka 注册中心</b>
+ * @author
+ * @date
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class WitmedRegistryStarter {
+	public static void main(String[] args) {
+		SpringApplication.run(WitmedRegistryStarter.class, args);
+	}
+}
